7aeeb371ac1c7a5baafa64f1a8ce064365aaf854
Nonsocha <162307332+Nonsocha@users.noreply.github.com>
2025-08-12 22:06:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./server');
-const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
