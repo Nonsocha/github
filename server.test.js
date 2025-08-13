a8f1cf20d1d326951188dd71c69892182fe37abb
Nonsocha <162307332+Nonsocha@users.noreply.github.com>
2025-08-13 14:33:51 +0100

    Update server.test.js

--- server.test.js
+++ server.test.js
@@ -1,10 +1,1 @@
-const request = require('supertest');
-const app = require('./server');
 
-describe('GET /', () => {
-  it('should return Hello World', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello World');
-  });
-});
